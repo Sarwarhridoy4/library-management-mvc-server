2d707b1ed48173f8c494049c952a3c2e263cef83
Sarwar Hossain <sarwarhridoy4@gmail.com>
2025-07-02 14:37:38 +0600

    deployed to vercel

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
-    message: "Welcome to the Book Borrowing API",
+    message: "Welcome to the Library Management API...",
   });
 });
 
