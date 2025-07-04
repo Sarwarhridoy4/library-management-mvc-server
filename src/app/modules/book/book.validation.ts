5ce2fec2d62a3f9fceba293fa45a258e8d381989
Sarwar Hossain <sarwarhridoy4@gmail.com>
2025-07-05 05:12:57 +0600

    remove constrains of timited genre

--- src/app/modules/book/book.validation.ts
+++ src/app/modules/book/book.validation.ts
@@ -4,19 +4,12 @@
 
 import { z } from "zod";
 
-export const genreEnum = z.enum([
-  "FICTION",
-  "NON_FICTION",
-  "SCIENCE",
-  "HISTORY",
-  "BIOGRAPHY",
-  "FANTASY",
-]);
+
 
 export const createBookZodSchema = z.object({
   title: z.string({ required_error: "Title is required" }),
   author: z.string({ required_error: "Author is required" }),
-  genre: genreEnum,
+  genre: z.string({ required_error: "Genre is required" }),
   isbn: z.string({ required_error: "ISBN is required" }),
   description: z.string().optional(),
   copies: z.number({ required_error: "Copies is required" }).int().min(0),
