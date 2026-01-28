949e2d78a182787ebc75c406e0b8ed920fef7522
Sarwar Hossain <sarwarhridoy4@gmail.com>
2026-01-28 15:01:26 +0600

    added workfliow for auto merge and deploy

--- src/app/middleware/errorHandler.ts
+++ src/app/middleware/errorHandler.ts
@@ -4,7 +4,6 @@ export const globalErrorHandler: ErrorRequestHandler = (
   err,
   _req,
   res,
-  _next
 ) => {
   const statusCode = 400;
   const message = err.message || "Something went wrong";
